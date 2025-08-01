ce48ef5963ddfcb23164adfe46a5ba09295c1fdd
DanielDAAR <147014618+DanielDAAR@users.noreply.github.com>
2025-08-01 01:23:56 -0600

    Create productos.js

---
+++ productos.js
@@ -0,0 +1,17 @@
+const productos = [
+  {
+    nombre: "Vape Cloud X100",
+    precio: "$799",
+    imagen: "https://via.placeholder.com/200x200?text=Cloud+X100"
+  },
+  {
+    nombre: "Smok Mini Pro",
+    precio: "$599",
+    imagen: "https://via.placeholder.com/200x200?text=Smok+Mini"
+  },
+  {
+    nombre: "Sky Vape 9000",
+    precio: "$899",
+    imagen: "https://via.placeholder.com/200x200?text=Sky+Vape+9000"
+  }
+];
