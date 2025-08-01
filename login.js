db857a8a627acddb9e0c604dc5d7e708c2be7ae2
DanielDAAR <147014618+DanielDAAR@users.noreply.github.com>
2025-08-01 11:27:17 -0600

    Create login.js

---
+++ login.js
@@ -0,0 +1,13 @@
+document.getElementById("ingresar").addEventListener("click", () => {
+  const user = document.getElementById("usuario").value;
+  const pass = document.getElementById("contraseña").value;
+
+  const adminUser = "admin";
+  const adminPass = "1234";
+
+  if (user === adminUser && pass === adminPass) {
+    window.location.href = "admin.html";
+  } else {
+    document.getElementById("mensaje").textContent = "Usuario o contraseña incorrectos";
+  }
+});
